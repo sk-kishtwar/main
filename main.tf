be69e801136612de9b7b3e4d1d97838211c0c440
suresh <cloudeng65@gmail.com>
2021-12-22 11:19:49 +0530

    vpc has been created

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  credentials = "${file("service-account.json")}"
+  project     = "avid-task-325512"
+  region  = "us-central1"  
+  zone        = "us-central1-b"
+}
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-network2"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
